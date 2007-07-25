b52595f2f4f2baca147e9f25f6a547b7eea5a781
Emmanuele Bassi <ebassi@openedhand.com>
2007-07-25 20:30:53 +0000

    Update the boxes example
    Add a better key and button presses event handlers, and exercise part
    of the events API.

--- examples/boxes.pl
+++ examples/boxes.pl
@@ -3,12 +3,36 @@
 use warnings;
 use strict;
 
+use Data::Dumper;
+
 use Glib qw( :constants );
 use Clutter qw( :init );
+use Clutter::Keysyms;
+
+sub button_press_cb {
+    my ($stage, $event) = @_;
+
+    my ($x, $y) = $event->get_coords();
+    print "*** Button ", $event->button, " pressed at ", $x, ", ", $y, "\n";
+
+    my $actor = $stage->get_actor_at_pos ($x, $y);
+
+    print Dumper($actor);
+}
+
+sub key_press_cb {
+    my ($stage, $event) = @_;
+
+    Clutter->main_quit() if $event->symbol eq $Clutter::Keysyms{Escape};
+
+    print "*** Key ", $event->symbol,
+          " ('", chr($event->unicode) , "') pressed\n";
+}
 
 my $stage = Clutter::Stage->get_default();
 $stage->set_color(Clutter::Color->new(0x00, 0x00, 0x00, 0xff));
-$stage->signal_connect(key_press_event => sub { Clutter->main_quit(); });
+$stage->signal_connect(button_press_event => \&button_press_cb);
+$stage->signal_connect(key_press_event    => \&key_press_cb);
 
 my $vbox = Clutter::VBox->new();
 $vbox->set_position(100, 100);
