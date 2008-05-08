6e77c60a6052c2da8a73c58d05f3f04bb60388e2
Emmanuele Bassi <ebassi@openedhand.com>
2008-05-08 15:51:59 +0000

    2008-05-08  Emmanuele Bassi  <ebassi@openedhand.com>
    	* Clutter.pm: Bump to 0.621
    
    	* README:
    	* NEWS: Release 0.621

--- Clutter.pm
+++ Clutter.pm
@@ -37,7 +37,7 @@ our @ISA = qw( DynaLoader Exporter );
 # of the bindings for each point release of libclutter,
 # which should be enough even in case of brown paper
 # bag releases. -- ebassi
-our $VERSION = '0.620';
+our $VERSION = '0.621';
 
 sub import {
     my $class = shift;
