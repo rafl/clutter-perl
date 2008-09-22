747c1b24fac188e03d1fc5ec403523fc8e0b4ab1
Emmanuele Bassi <ebassi@openedhand.com>
2008-09-22 13:04:28 +0000

    Release 0.802

--- Clutter.pm
+++ Clutter.pm
@@ -38,7 +38,7 @@ our @ISA = qw( DynaLoader Exporter );
 # of the bindings for each point release of libclutter,
 # which should be enough even in case of brown paper
 # bag releases. -- ebassi
-our $VERSION = '0.801';
+our $VERSION = '0.802';
 
 sub import {
     my $class = shift;
