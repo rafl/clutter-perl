fac5a4d2d7776da8fad28b30ce6edd8b9dcbe9dc
Emmanuele Bassi <ebassi@linux.intel.com>
2008-12-14 18:46:24 +0000

    [docs] Document the TextureReflection example
    Like Clutter::Ex::Triangle, the TextureReflection actor needs a
    perldoc page.

--- examples/reflection.pl
+++ examples/reflection.pl
@@ -6,10 +6,49 @@ use strict;
 use Glib qw( :constants );
 use Clutter;
 
+=pod
+
+=head1 NAME
+
+Clutter::Ex::TextureReflection - Reflection of a texture
+
+=head1 SYNOPSIS
+
+  use Clutter qw( :init );
+  use Clutter::Ex::TextureReflection;
+
+  my $texture = Clutter::Texture->new($filename);
+
+  my $reflection = Clutter::Ex::TextureReflection->new();
+  $reflection->set_parent_texture($texture);
+
+=head1 DESCRIPTION
+
+This page describes the API of C<Clutter::Ex::TextureReflection>, a subclass
+of L<Clutter::Texture::Clone> that paints a reflection of the parent texture.
+
+=head1 HIERARCHY
+
+  Glib::Object
+  +----Glib::InitiallyUnowned
+       +----Clutter::Actor
+            +----Clutter::Texture::Clone
+                 +----Clutter::Ex::TextureReflection
+
+=cut
+
 use Glib::Object::Subclass
     'Clutter::Texture::Clone',
     signals => { },
-    properties => [ ];
+    properties => [
+        Glib::ParamSpec->int(
+            'reflection-height',
+            'Reflection Height',
+            'The height of the reflection, or -1',
+            -1, 65536, -1,
+            [ qw( readable writable ) ],
+        ),
+    ];
 
 sub PAINT {
     my ($self) = @_;
@@ -46,11 +85,48 @@ sub PAINT {
     Clutter::Cogl->pop_matrix();
 }
 
+sub SET_PROPERTY {
+    my ($self, $pspec, $value) = @_;
+
+    $self->set_reflection_height($value)
+        if ($pspec->name() eq 'reflection-height');
+}
+
+sub GET_PROPERTY {
+    my ($self, $pspec) = @_;
+
+    return $self->get_reflection_height()
+        if ($pspec->name() eq 'reflection-height');
+}
+
 sub INIT_INSTANCE {
     my ($self) = @_;
 
     $self->{reflection_height} = -1;
 }
+
+=pod
+
+=head1 METHODS
+
+=over
+
+=item B<< actor = Clutter::Ex::TextureReflection->new >>
+
+Creates a new Clutter::Ex::TextureReflection actor.
+
+=cut
+
+=pod
+
+=item B<< $reflection->set_reflection_height ($height) >>
+
+  * $height (integer)
+
+Sets the height of the reflection, in pixels. If I<height> is a negative
+value, the height of the parent texture will be used instead.
+
+=cut
 
 sub set_reflection_height {
     my ($self, $height) = @_;
@@ -58,16 +134,56 @@ sub set_reflection_height {
     $self->{reflection_height} = $height;
 }
 
+=pod
+
+=item B<< height = $reflection->get_reflection_height >>
+
+Retrieves the height of the reflection.
+
+=cut
+
 sub get_reflection_height {
     my ($self) = @_;
 
     return $self->{reflection_height};
 }
 
+=pod
+
+=back
+
+=head1 PROPERTIES
+
+=over
+
+=item 'reflection-height' (integer : readable / writable)
+
+Height of the reflection, in pixels
+
+=back
+
+=head1 SEE ALSO
+
+L<Clutter>, L<Glib::Object>, L<Glib::InitiallyUnowned>, L<Clutter::Actor>,
+L<Clutter::Texture>, L<Clutter::Texture::Clone>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2008  Intel Corporation.
+
+This module is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License version 2.1, or
+under the terms of the Artistic License. See Clutter for the full copyright
+notice.
+
+=cut
+
 package main;
 
 use Glib qw( :constants );
 use Clutter qw( :init );
+
+my $filename = defined $ARGV[0] ? $ARGV[0] : 'redhand.png';
 
 my $stage = Clutter::Stage->new();
 $stage->set_size(640, 480);
@@ -79,7 +195,7 @@ my $group = Clutter::Group->new();
 $stage->add($group);
 
 my $tex;
-eval { $tex = Clutter::Texture->new($ARGV[0]) };
+eval { $tex = Clutter::Texture->new($filename) };
 if ($@) {
     warn ("Unable to load '$ARGV[0]': $@");
     exit 1;
