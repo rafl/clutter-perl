91b2bbfe577b50b083f10817c961adb3943aad0b
Emmanuele Bassi <ebassi@linux.intel.com>
2009-09-04 16:27:33 +0100

    [examples] Make the 'hello world' more fun
    The obligatory 'hello world' for a library like Clutter should be more
    fun than a plain string on a green blackground.

--- examples/hello.pl
+++ examples/hello.pl
@@ -10,6 +10,10 @@
 use strict;
 use warnings;
 
+use constant FONT_DESC  => 'Sans 96px';
+
+use Math::Trig qw( :pi );
+use Glib qw( :constants );
 use Clutter qw( :init );
 use Clutter::Keysyms;
 
@@ -23,13 +27,85 @@ $stage->signal_connect('key-press-event'    => sub {
         if ($event->keyval == $Clutter::Keysyms{Escape});
 });
 $stage->set_size(800, 600);
+$stage->show();
 
-my $label = Clutter::Text->new("Sans 30", "Hello, Clutter!");
-$label->set_color(Clutter::Color->new(0xff, 0xcc, 0xcc, 0xdd));
-$label->set_position(($stage->get_width()  - $label->get_width())  / 2,
-                     ($stage->get_height() - $label->get_height()) / 2);
-$stage->add($label);
-$stage->show_all();
+my $timeline = Clutter::Timeline->new(3000);
+$timeline->set_loop(TRUE);
+
+my $alpha = Clutter::Alpha->new($timeline);
+$alpha->set_func(sub {
+    return sin($alpha->get_timeline()->get_progress() * pi);
+});
+
+my $x_offset = 64;
+my $y_offset = $stage->get_height() / 2;
+
+my @str = split(//, 'hello, clutter!', -1);
+my $idx = 0;
+foreach my $char (@str) {
+    my $color = Clutter::Color->new(
+        int(rand(255)),
+        int(rand(255)),
+        int(rand(255)),
+        255,
+    );
+    my $text = Clutter::Text->new(FONT_DESC, "$char", $color);
+
+    $text->set_position($x_offset, $y_offset);
+    $stage->add($text);
+
+    my $behaviour;
+
+    if ($idx % 7 == 0) {
+        $behaviour = Clutter::Behaviour::Opacity->new(
+            $alpha,
+            255, int(rand(64)),
+        );
+    }
+    elsif ($idx % 3 == 0) {
+        $behaviour = Clutter::Behaviour::Rotate->new(
+            $alpha,
+            'z-axis',
+            ($idx % 2) ? 'cw' : 'ccw',
+            0.0, 0.0,
+        );
+        $behaviour->set_center(
+            $text->get_width(),
+            $text->get_height(),
+            0,
+        );
+    }
+    elsif ($idx % 2 == 0) {
+        my $final = ($idx % 5) ? 1.0 + rand(1.0) : rand(0.8);
+        $behaviour = Clutter::Behaviour::Scale->new(
+            $alpha,
+            1.0,    1.0,
+            $final, $final,
+        );
+        $text->move_anchor_point_from_gravity('center');
+    }
+    else {
+        my ($x, $y) = (int(rand(50)), int(rand(50)));
+        $behaviour = Clutter::Behaviour::Path->new(
+            $alpha,
+            Clutter::Path->new(
+                [ 'move-to', [ [ $x_offset     , $y_offset      ] ] ],
+                [ 'line-to', [ [ $x_offset + $x, $y_offset - $y ] ] ],
+                [ 'line-to', [ [ $x_offset + $x, $y_offset + $y ] ] ],
+                [ 'line-to', [ [ $x_offset - $x, $y_offset + $y ] ] ],
+            ),
+        );
+    }
+
+    $behaviour->apply($text);
+    $text->{behaviour} = $behaviour;
+
+    $x_offset += $text->get_width() - 5;
+
+    $idx += 1;
+}
+
+$timeline->start();
 
 Clutter->main();
 
